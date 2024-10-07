89083b8c4f71ebb44bf31ff37820f57e2e6ff09c
Taís Fonseca <161239074+taisfonsec@users.noreply.github.com>
2024-10-07 18:48:24 -0300

    Add files via upload

---
+++ CursoemVideo/ex001.py
@@ -0,0 +1,2 @@
+nome = input('Qua é o seu nome? ')
+print('É um grande prazer te conhecer', nome)
---
+++ scripts-python/desafio03.py
@@ -0,0 +1,4 @@
+n1 = input('Primeiro némero ')
+n2 = input('Segundo número ')
+print('A soma é ', n1 + n2)
+#erro proposital

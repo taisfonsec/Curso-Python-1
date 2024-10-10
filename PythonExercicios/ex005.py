f5757eddbd29c9543d4dd6dbee381cb3373973bf
Taís Fonseca <161239074+taisfonsec@users.noreply.github.com>
2024-10-10 11:05:11 -0300

    Delete PythonExercicios directory

--- PythonExercicios/ex005.py
+++
@@ -1,2 +0,0 @@
-n = int(input('Digite um número: '))
-print('O seu sucessor é {} e seu antecessor é {}'.format(n + 1, n - 1))
--- PythonExercicios/ex007.py
+++
@@ -1,3 +0,0 @@
-n1 = float(input('Digite a primeira nota: '))
-n2 = float(input('Digite a segunda nota: '))
-print('A media do aluno é {:.2f}'.format((n1 + n2) / 2))

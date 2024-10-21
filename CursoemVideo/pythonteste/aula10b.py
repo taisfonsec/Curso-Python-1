be07311ab2bffd283383d19c1e7a78af29ca3bd3
Taís Fonseca <161239074+taisfonsec@users.noreply.github.com>
2024-10-21 14:32:32 -0300

    Add files via upload

---
+++ CursoemVideo/pythonteste/aula10b.py
@@ -0,0 +1,13 @@
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m = (n1 + n2) / 2
+
+print('A sua média foi {:.1f}'.format(m))
+print('PARABÉNS' if m >= 6 else 'ESTUDE MAIS!')
+
+'''
+if m >= 6:
+    print('Sua média foi boa! PARABÉNS!')
+else:
+    print('Sua média foi ruim! ESTUDE MAIS!')
+'''

f0d8f81a42dfbb8f9efda90a7ed5a62af117a35a
Taís Fonseca <161239074+taisfonsec@users.noreply.github.com>
2024-12-24 15:23:04 -0300

    Add files via upload

--- CursoemVideo/PythonExercicios/ex039.py
+++ CursoemVideo/PythonExercicios/ex039.py
@@ -1,18 +1,27 @@
 from datetime import date
 
-anoAtual = date.today().year
-anoNascimento = int(input('Digite o ano de nascimento: '))
-idade = anoAtual - anoNascimento
+genero = str(input('''[M] Masculino
+[F] Feminino
+Digite o gênero: '''))
 
-print('O jovem tem {} anos em {}'.format(idade, anoAtual))
+if genero.upper() == 'M':
+    anoAtual = date.today().year
+    anoNascimento = int(input('Digite o ano de nascimento: '))
+    idade = anoAtual - anoNascimento
 
-if idade == 18:
-    print('Você deve se alistar imediatamente!')
-elif idade < 18:
-    anosFaltando = 18 - idade
-    print('Você ainda não tem 18 anos. Ainda faltam {} anos para o alistamento'.format(anosFaltando))
-    print('Seu alistamento será em {}'.format(anoAtual + anosFaltando))
-elif idade > 18:
-    anosPassados = idade - 18
-    print('Você já deveria ter se alistado há {} anos'.format(anosPassados))
-    print('Seu alistamento foi em {}'.format(anoAtual - anosPassados))
+    print('O jovem tem {} anos em {}'.format(idade, anoAtual))
+
+    if idade == 18:
+        print('Você deve se alistar imediatamente!')
+    elif idade < 18:
+        anosFaltando = 18 - idade
+        print('Você ainda não tem 18 anos. Ainda faltam {} anos para o alistamento'.format(anosFaltando))
+        print('Seu alistamento será em {}'.format(anoAtual + anosFaltando))
+    elif idade > 18:
+        anosPassados = idade - 18
+        print('Você já deveria ter se alistado há {} anos'.format(anosPassados))
+        print('Seu alistamento foi em {}'.format(anoAtual - anosPassados))
+elif genero.upper() == 'F':
+    print('Você não precisa fazer alistamento militar obrigatório!')
+else:
+    print('Opção inválida! Tente novamente.')
